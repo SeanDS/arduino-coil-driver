95f042de1cde653fc556353304469bfca3f52fe1
Sean Leavey <sean.leavey@gmail.com>
2015-06-28 18:44:33 +0100

    Some minor tweaks

--- arduino/CoilDriver/CoilDriver.ino
+++ arduino/CoilDriver/CoilDriver.ino
@@ -6,7 +6,7 @@
 * Sean Leavey
 * s.leavey.1@research.gla.ac.uk
 * June 2015
-* 
+*
 * https://github.com/SeanDS/arduino-coil-driver/
 */
 
@@ -74,61 +74,61 @@ void setup()
 {
   // open serial communications and wait for port to open:
   Serial.begin(9600);
-  
+
   // set analogue write resolution to 8-bits, which is the fundamental limit of the Due's PWM outputs
   analogWriteResolution(8);
-  
+
   // set slave select pins to output (ethernet and SD card reader)
   //pinMode(SD_SS_PIN, OUTPUT); // weirdly, with this enabled, the program crashes, but it works without it being used
   pinMode(ETHERNET_SS_PIN, OUTPUT);
-  
+
   // toggle SD card on, ethernet off
   switchSlaveSelect(true);
-  
+
   if (!SD.begin(SD_SS_PIN)) {
     Serial.println("SD card initialisation failed.");
-    
+
     // set outputs to default values (better than undefined values, which just output a floating voltage)
     for (int i = 0; i < NUMBER_OF_OUTPUTS; i++) {
       writePin(outputPins[i], DEFAULT_OUTPUT_VALUE);
     }
   } else {
     Serial.println("SD card initialised!");
-    
+
     // set card present
     sdcardPresent = true;
-    
+
     // search for saved pin values on SD card
     restoreSavedOutputLevels();
   }
-  
+
   // toggle ethernet shield on, SD card off
   switchSlaveSelect(false);
 
   // start server
   startServer();
-  
+
   // convert IP address and mac into strings (for sending to the controller as part of status messages)
   ipString = ipToString(Ethernet.localIP());
   macString = macToString(mac);
-  
+
   // initialise input pins
   pinMode(DIGITAL_INPUT_PIN_1, INPUT);
   pinMode(DIGITAL_INPUT_PIN_2, INPUT);
 }
 
 void loop()
-{
+{  
   loopCounter++;
-  
+
   if (loopCounter >= 666000) { // approximately 30s
       // send a status report to controller
-      
+
       Serial.println("Reporting in with server");
-      
+
       // initialise a client
       EthernetClient statusClient;
-      
+
       // connect and send message
       if (statusClient.connect(ipAddressController, 80)) {
           String message = messageEncode("{" + getStatusJSON() + "}");
@@ -139,53 +139,53 @@ void loop()
       } else {
           Serial.println("Status report failed");
       }
-    
+
       // reset loop counter
       loopCounter = 0;
   }
-  
+
   // listen for incoming client
   EthernetClient client = server.available();
 
   if (client)
   {
     Serial.println("New client connection");
-    
+
     // get request
     String request = getClientRequest(client);
-    
+
     // handle the request
     handleRequest(request, client);
-    
+
     // close the connection
     client.stop();
-    
+
     Serial.println("Client disconnected");
   }
 }
 
-String getClientRequest(EthernetClient client) {  
+String getClientRequest(EthernetClient client) {
   String request;
-  
+
   // an http request ends with a blank line, so keep track of this.
   boolean currentLineIsBlank = true;
-  
+
   while (client.connected())
   {
     if (client.available())
     {
       // read next character from client's request
       char c = client.read();
-      
+
       // add this character to the request string
       request += c;
-      
+
       // an end of line character on an otherwise blank line tells us the client's request has ended, so send a reply
       if (c == '\n' && currentLineIsBlank)
       {
         break;
       }
-      
+
       // if we get this far, we've not reached the end of a request,
       // so check if we're at the end of a line instead
       if (c == '\n')
@@ -212,38 +212,38 @@ void restoreSavedOutputLevels() {
     String filename = "PIN"; // capital letters in filename required by FAT16
     filename += String(outputPins[i]);
     filename += ".TXT"; // the three character extension is required by FAT16
-    
+
     // convert String object to char array, because the SD library doesn't support Strings
     char filenameChar[filename.length() + 1];
     filename.toCharArray(filenameChar, sizeof(filenameChar));
-    
+
     if (SD.exists(filenameChar)) {
       // saved value file found, so read it
       File filePointer = SD.open(filenameChar, FILE_READ);
-      
+
       if (filePointer) {
         Serial.println("Found value file for pin " + String(outputPins[i]));
-        
+
         char message[3];
-        
+
         int j = 0;
-        
+
         // read first 3 characters
         while (filePointer.available() && j < 3) {
           message[j] = filePointer.read();
-          
+
           j++;
         }
-      
+
         // convert char array to int
         int value = atoi(message);
-        
+
         // sanity check the value - make sure it's within range
         sanitiseValue(value);
-    
+
         // set the appropriate pin's value
         writePin(outputPins[i], value);
-        
+
         filePointer.close();
       } else {
         Serial.println("Couldn't open file even though it exists. Using default value.");
@@ -251,7 +251,7 @@ void restoreSavedOutputLevels() {
       }
     } else {
       Serial.println("Couldn't find value file for pin " + String(outputPins[i]) + ". Using default value.");
-      
+
       writePin(outputPins[i], DEFAULT_OUTPUT_VALUE);
     }
   }
@@ -267,13 +267,13 @@ void sanitiseValue(int &value) {
 
 void startServer() {
   Serial.println("Starting Ethernet server...");
-  
+
   // start the ethernet connection and the server
   Ethernet.begin(mac);
   server.begin();
-  
+
   Serial.println("Server started");
-  
+
   // print server IP address
   Serial.print("IP address: ");
   Serial.println(Ethernet.localIP());
@@ -281,7 +281,7 @@ void startServer() {
 
 String ipToString(IPAddress ipAddress) {
   String ipString = String(ipAddress[0]);
-  
+
   for (int i = 1; i < 4; i++) {
     ipString += "." + String(ipAddress[i]);
   }
@@ -291,7 +291,7 @@ String ipToString(IPAddress ipAddress) {
 
 String macToString(byte mac[]) {
   macString = String(mac[0]);
-  
+
   for (int i = 1; i < 6; i++) {
     macString += ":" + String(mac[i]);
   }
@@ -302,7 +302,7 @@ String macToString(byte mac[]) {
 void switchSlaveSelect(boolean sdcard) {
   boolean sdPin;
   boolean ethernetPin;
-  
+
   if (sdcard) {
     sdPin = LOW;
     ethernetPin = HIGH;
@@ -310,26 +310,26 @@ void switchSlaveSelect(boolean sdcard) {
     sdPin = HIGH;
     ethernetPin = LOW;
   }
-  
+
   digitalWrite(SD_SS_PIN, sdPin);
   digitalWrite(ETHERNET_SS_PIN, ethernetPin);
 }
 
 void handleRequest(String request, EthernetClient client) {
   Serial.println("Request: " + request);
-  
+
   /*
    * Types of request:
    *
    * GET /status      Returns a status message for this Arduino, with IP, MAC, etc.
    * GET /outputs     Returns a list of each of this Arduino's outputs and corresponding values
    * GET /toggle [PIN] [VALUE]  Toggles pin [PIN] output to value [VALUE]. [PIN] must be two characters
-   *        long, so pin 2 should be specified as 02. 
+   *        long, so pin 2 should be specified as 02.
    */
-  
+
   // parse the requested information
   if (request.indexOf("GET /status") > -1)
-  {                                             
+  {
     sendStatus(client);
   } else if (request.indexOf("GET /outputs") > -1) {
     sendOutputs(client);
@@ -373,19 +373,19 @@ void sendOutputs(EthernetClient client) {
   sendJsonReplyHeaders(client);
 
   StaticJsonBuffer<1024> jsonBuffer;
-  
+
   JsonObject& root = jsonBuffer.createObject();
 
   // create list of key names
   String keyNames[NUMBER_OF_OUTPUTS];
-  
+
   for (int i = 0; i < NUMBER_OF_OUTPUTS; i++) {
     keyNames[i] = String("pin_") + String(outputPins[i]);
   }
 
   // now add these key names to the JSON array with their values
   // this has to be done this way because of https://github.com/bblanchon/ArduinoJson/issues/84
-  for (int i = 0; i < NUMBER_OF_OUTPUTS; i++) {    
+  for (int i = 0; i < NUMBER_OF_OUTPUTS; i++) {
     root.set(keyNames[i], pinValues[i]);
   }
 
@@ -418,40 +418,69 @@ void sendErrorReplyHeaders(EthernetClient client) {
 boolean handleToggleRequest(String request) {
   int setIndex = request.indexOf("set=");
   int httpIndex = request.indexOf("HTTP/1.1");
-  
+
   if (setIndex > -1 && httpIndex > -1) {
     // get characters from "input=" to end of request
     String jsonString = request.substring(setIndex + 4, httpIndex);
-    
+
     Serial.println("Parsing string: " + jsonString);
-    
+
     // JSON buffer for receiving JSON messages
     StaticJsonBuffer<1024> jsonBuffer;
-    
+
     // parse JSON
     JsonObject& root = jsonBuffer.parseObject(jsonString);
-    
+
     if (! root.success()) {
       Serial.println("Failed to parse JSON object");
     } else {
-      Serial.println("Success!");
+      Serial.println("Successfully parsed JSON object");
 
+      //
+      // check that there is at least one pin toggle defined
+      //
+
+      // initialise list of found pins to false
+      boolean pinsFound[NUMBER_OF_OUTPUTS] = {LOW};
+
+      // initialise list of output settings mapped to the index of the output in outputPins array
+      unsigned int valuesToSet[NUMBER_OF_OUTPUTS];
+
+
+      //// passed in JSON list should have the following data:
+      /*
+       *   coarse pin setting
+       *   fine pin setting
+       *   mapping between coarse and fine (~63)
+       *   midpoint at which coarse and fine are equal (i.e. ~126)
+       *   
+       *   Then the algorithm should move FINE until it reaches the midpoint, then move 63 (or 62?) steps in whatever direction it needs to go
+       *   before then incrementing the COARSE output and simultaneously going back to the midpoint on FINE. Repeat until COARSE setting is reached
+       *   and then continue to set FINE until the desired FINE setting is reached.
+       */
+
+      
+
+      // loop over output pins
       for (int i = 0; i < NUMBER_OF_OUTPUTS; i++) {
-        int outputPin = outputPins[i];
-        
+        unsigned int outputPin = outputPins[i];
+
         if (root.containsKey("pin_" + String(outputPin))) {
+          // pin toggle request found
+          pinsFound[i] = HIGH;
+
+          // get the value
           int value = root["pin_" + String(outputPin)];
 
           // restrict value to bounds
           sanitiseValue(value);
-          
-          Serial.print("Setting " + String(outputPin) + String(" to ") + String(value) + String("..."));
-          rampToValue(outputPin, value, 100);
-          Serial.println("Done");
+
+          // store value
+          valuesToSet[i] = value;
         }
       }
     }
-    
+
     Serial.println("Finished parsing");
   } else {
     return false;
@@ -472,13 +501,13 @@ boolean handleToggleRequest(String request) {
 
 /*
  * Gets a status message formatted as JSON (but without the enclosed curly brackets).
- * 
+ *
  * DEPRECATED
  */
 String getStatusJSON()
 {
     reply = "\"sdcard\":";
-    
+
     if (sdcardPresent) {
         reply += "\"present\"";
     } else {
@@ -487,20 +516,20 @@ String getStatusJSON()
 
     // add coil contact information
     reply += ",\"coil_contact_1\":\"" + String(digitalRead(DIGITAL_INPUT_PIN_1))  + "\",\"coil_contact_2\":\"" + String(digitalRead(DIGITAL_INPUT_PIN_2)) + "\"";
-    
+
     // add other auxiliary information
     reply += ",\"mac\":\"" + macString  + "\",\"ip\":\"" + ipString + "\",\"version\":\"" + SOFTWARE_VERSION + "\"";
-    
+
     return reply;
 }
 
 /*
  * URL encodes a given char array for safe use with HTTP
- * 
+ *
  * DEPRECATED
  */
 String characterEncode(const char* message)
-{  
+{
     const char *hex = "0123456789abcdef";
     String encodedMessage = "";
 
@@ -514,23 +543,23 @@ String characterEncode(const char* message)
             encodedMessage += hex[*message >> 4];
             encodedMessage += hex[*message & 15];
         }
-        
+
         message++;
     }
-    
+
     return encodedMessage;
 }
 
 /*
  * URL encodes a given String object for safe use with HTTP
- * 
+ *
  * DEPRECATED
  */
 String messageEncode(String message)
 {
     char buffer[1024];
     message.toCharArray(buffer, 1024);
-    
+
     return characterEncode(buffer);
 }
 
@@ -545,7 +574,7 @@ char* getPinFilename(int pin)
 
   char filename[(sizeof(pinString) + 1) / sizeof(char)];
   pinString.toCharArray(filename, sizeof(filename));
-  
+
   return filename;
 }
 
@@ -560,21 +589,25 @@ void rampToValue(int pin, int newValue, int stepPause)
       int currentValue = pinValues[getPinPosition(pin)];
       int difference = newValue - currentValue;
 
+      Serial.print("Setting " + String(pin) + String(" from ") + String(currentValue) + String(" to ") + String(newValue) + String("..."));
+
       while (difference != 0) {
           if (difference > 0) {
               // set point is higher than current value
               currentValue++;
           } else {
-              // set point is lower than current value            
+              // set point is lower than current value
               currentValue--;
           }
-              
+          
           writePin(pin, currentValue);
-        
+
           delay(stepPause);
 
           difference = newValue - currentValue;
       }
+
+      Serial.println(" done");
    }
 }
 
@@ -582,7 +615,7 @@ void writePin(int pin, int value)
 {
       // change the output level
       analogWrite(pin, value);
-      
+
       // update pin value array with new value
       pinValues[getPinPosition(pin)] = value;
 }
@@ -597,7 +630,7 @@ int getPinPosition(int pin)
         return i;
       }
     }
-    
+
     return -1;
 }
 
@@ -612,6 +645,6 @@ boolean pinExists(int pin)
       return true;
     }
   }
-  
+
   return false;
 }
