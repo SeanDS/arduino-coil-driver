1ce8a3f9b90d7356f5c79a33576cd8ab4854151e
Sean Leavey <sean.leavey@gmail.com>
2015-10-22 22:58:08 +0100

    Split driver output adjustment template into multiple templates

---
+++ server/templates/dashboard-control-output-script.php
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+    // initialise mode switches
+    $(document).ready(
+        function() {
+            $(".switchramp").each(
+                function() {
+                    $(this).bootstrapSwitch()
+                }
+            )
+        }
+    );
+</script>
