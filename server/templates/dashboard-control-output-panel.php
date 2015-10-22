1f5a5b7896496a3e1d3ed9e1ee362dbd2e1ba3ee
Sean Leavey <sean.leavey@gmail.com>
2015-10-22 23:55:44 +0100

    Javascript for output adjustment

--- server/templates/dashboard-control-output-panel.php
+++ server/templates/dashboard-control-output-panel.php
@@ -7,45 +7,45 @@
             <div class="col-md-3">
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">-1</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="-1" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">-1</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">-5</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="-5" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">-5</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">-20</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="-20" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">-20</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">-100</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="-100" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">-100</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">-500</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="-500" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">-500</button>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
-                <p class="pin-value text-center"><?=$this->e($driverOutput->getValue())?></p>
+                <p class="pin-value text-center driver-output-adjust-current-value" data-driver-output-id="<?=$this->e($driverOutput->getId())?>"><?=$this->e($driverOutput->getValue())?></p>
                 <form>
                     <div class="form-group">
                         <div class="input-group">
                             <div class="input-group-addon">Set to</div>
-                            <input class="form-control" type="text" name="custom" id="custom" placeholder="0" maxlength="5" value="0"/>
+                            <input class="form-control driver-output-adjust-set-text" type="text" name="custom" id="custom" placeholder="0" maxlength="5" value="0" data-driver-output-id="<?=$this->e($driverOutput->getId())?>"/>
                             <span class="input-group-btn">
-                                <button class="btn btn-primary" type="button">Go</button>
+                                <button class="btn btn-primary driver-output-adjust-set-button" type="button" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">Go</button>
                             </span>
                         </div>
                     </div>
                     <div class="form-group">
                         <p>
-                            <input class="switchramp" type="checkbox" data-on-text="Ramp" data-off-text="Snap" data-handle-width="100" data-label-text="Mode" checked>
+                            <input class="switchramp" type="checkbox" data-on-text="Ramp" data-off-text="Snap" data-handle-width="100" data-label-text="Mode" data-driver-output-id="<?=$this->e($driverOutput->getId())?>" checked>
                         </p>
                     </div>
                 </form>
@@ -56,27 +56,27 @@
             <div class="col-md-3">
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">+1</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="1" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">+1</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">+5</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="5" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">+5</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">+20</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="20" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">+20</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">+100</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="100" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">+100</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <button class="btn btn-default btn-sm btn-block">+500</button>
+                        <button class="btn btn-default btn-sm btn-block driver-output-adjust-button" data-offset="500" data-driver-output-id="<?=$this->e($driverOutput->getId())?>">+500</button>
                     </div>
                 </div>
             </div>
--- server/templates/dashboard-control-output-script.php
+++ server/templates/dashboard-control-output-script.php
@@ -2,11 +2,112 @@
     // initialise mode switches
     $(document).ready(
         function() {
+            // enable fancy switch buttons on driver outputs
             $(".switchramp").each(
                 function() {
                     $(this).bootstrapSwitch()
                 }
-            )
+            );
+            
+            // set callbacks on driver output adjustment buttons
+            $(".driver-output-adjust-button").each(
+                function() {
+                    $(this).click(
+                        function() {
+                            // get driver output
+                            var driverOutputId = $(this).data("driverOutputId");
+                        
+                            // get adjustment offset
+                            var offset = $(this).data("offset");
+                            
+                            // get current value
+                            var currentValue = getDriverOutputValue(driverOutputId);
+                            
+                            // toggle output
+                            toggleOutput(driverOutputId, currentValue + offset);
+                        }
+                    );
+                }
+            );
+            
+            // set callbacks on driver output adjustment textareas
+            $(".driver-output-adjust-set-button").each(
+                function() {
+                    $(this).click(
+                        function() {
+                            // get driver output
+                            var driverOutputId = $(this).data("driverOutputId");
+                            
+                            // get adjustment value
+                            var value = getSetToValue(driverOutputId);
+                            
+                            // toggle output
+                            toggleOutput(driverOutputId, value);
+                        }
+                    );
+                }
+            );
         }
     );
+    
+    // toggles the output
+    function toggleOutput(driverOutputId, value) {
+        // get toggle mode
+        var toggleMode = getSwitchToggleMode(driverOutputId);
+        
+        console.log("Driver output ID: " + driverOutputId);
+        console.log("Value: " + value);
+        console.log("Toggle mode: " + toggleMode);
+    }
+    
+    // finds the toggle mode of the specified output ID
+    function getSwitchToggleMode(driverOutputId) {
+        var toggleMode = false;
+    
+        $(".switchramp").each(
+            function() {
+                if ($(this).data("driverOutputId") === driverOutputId) {
+                    toggleMode = $(this).bootstrapSwitch("state");
+                    
+                    return;
+                }
+            }
+        );
+        
+        return toggleMode;
+    }
+    
+    // finds the value of the driver output
+    function getDriverOutputValue(driverOutputId) {
+        var value = null;
+        
+        $(".driver-output-adjust-current-value").each(
+            function() {
+                if ($(this).data("driverOutputId") === driverOutputId) {
+                    value = parseInt($(this).text(), 10);
+                    
+                    return;
+                }
+            }
+        );
+        
+        return value;
+    }
+    
+    // finds the value in the manual adjustment text box
+    function getSetToValue(driverOutputId) {
+        var value = null;
+        
+        $(".driver-output-adjust-set-text").each(
+            function() {
+                if ($(this).data("driverOutputId") === driverOutputId) {
+                    value = parseInt($(this).val(), 10);
+                    
+                    return;
+                }
+            }
+        );
+        
+        return value;
+    }
 </script>
